5714abf7fc7071bd6a8226c5eccf47bfebf9def3
JSamuraiL <alex.ermiloff2017@yandex.ru>
2024-04-06 11:10:21 +0300

    Создание класса цветной техники

---
+++ Devices_with_color.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public abstract class Devices_with_color //Абстрактный класс цветной техники
+    {
+        public string color {  get; set; } //Цвет техники
+    }
+}
