802fb71da718bb3e9605c4d2a528db0a3039a112
JSamuraiL <alex.ermiloff2017@yandex.ru>
2024-04-06 13:13:29 +0300

    Создание СВЧ - печи

---
+++ Microwave.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Microwave : Appliances, electrical_appliances, multi_mode_technology
+    {
+        public string energy_class { get; set; }
+        public string Manufacturer { get; set; }
+        int multi_mode_technology.number_of_modes { get; set; }
+        public int Power { get; set; }
+    }
+}
