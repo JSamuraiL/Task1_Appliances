d4d6874fa42619d94ed2e9b62ae2e8247b2a3fc0
JSamuraiL <alex.ermiloff2017@yandex.ru>
2024-04-06 13:19:37 +0300

    Создание посудомойки

---
+++ Dishwasher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Dishwasher : Appliances, multi_mode_technology, electrical_appliances
+    {
+        public string energy_class { get; set; }
+        public string Manufacturer { get; set; }
+        int multi_mode_technology.number_of_modes { get; set; }
+    }
+}
