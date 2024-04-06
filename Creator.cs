fa8e1f1f4fc2799ccf7a87ed7535bbc11db6a369
JSamuraiL <alex.ermiloff2017@yandex.ru>
2024-04-06 08:57:30 +0300

    Create class

--- Creator.cs
+++ Creator.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Task1
 {
-    public abstract class Creator
+    public  class Creator
     {
         public string Manufacturer {  get; set; }
     }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Task1;
+
 Console.WriteLine("Hello, World!");
+var creator = new Creator
+{
+    Manufacturer = "Me"
+};
+Console.WriteLine(creator.Manufacturer);
