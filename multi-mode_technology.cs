16b2ad372d3c2c3b4e062b516fe540dd23ceb266
JSamuraiL <alex.ermiloff2017@yandex.ru>
2024-04-06 11:19:34 +0300

    Создание класса многорежимной техники

---
+++ multi-mode_technology.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Task1
+{
+    public abstract class multi_mode_technology //Класс многорежимной техники
+    {
+        public int number_of_modes {  get; set; } //Количество режимов
+    }
+}
